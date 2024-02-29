bc6e92bec291fa11ac8faf106552d76c3a093ed9
NaiM_AyouB <ayoubnaim833@gmail.com>
2024-02-29 12:48:02 -0800

    update task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL, and displays the body of the response.
-ponse body for a given URL for 200 status code responses.
+# Get the response body for a given URL for 200 status code responses.
 curl -sL "$1"
